e2e7ccd52ae3f046121bc4cf7e57b0570cbc458f
Sandra Ivanova <sandraivanova1@yahoo.com>
2025-11-02 18:25:59 +0100

    Initial commit

---
+++ src/main/java/mk/ukim/finki/wp/lab/Lab1WebProgramingApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1WebProgramingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1WebProgramingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/mk/ukim/finki/wp/lab/model/Chef.java
@@ -0,0 +1,2 @@
+package mk.ukim.finki.wp.lab.model;public class Chef {
+}
